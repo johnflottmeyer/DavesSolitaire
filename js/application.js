3527702c1426456ac0c279b2cbcb9cffbb1f278e
john <john@voilamediagroup.com>
2016-09-26 14:18:11 -0500

    updating the object code and styles

--- js/application.js
+++ js/application.js
@@ -13,7 +13,7 @@ solitaire to play on Saturday mornings.
 var suits = new Array("H","D","C","S");//suits
 var startDeck = new Array("A","2","3","4","5","6","7","8","9","10","J","Q","K");//all possible cards
 var shuffledDeck = [];
-var startCard = "";
+var startCard = [];
 var line0 = [];
 var line1 = [];
 var line2 = [];
@@ -27,24 +27,31 @@ var suitFour = "";
 removeByIndex = function(arr,index) {
 	arr.splice(index, 1);
 };
+function objectFindByKey(array, key, value) {
+    for (var i = 0; i < array.length; i++) {
+        if (array[i][key] === value) {
+            return array[i];
+        }
+    }
+    return null;
+}
 function resetVariables(){
-	var shuffledDeck = [];
-	var startCard = "";
-	var line0 = [];
-	var line1 = [];
-	var line2 = [];
-	var line3 = [];
-	var suitOne = "";
-	var suitTwo = "";
-	var suitThree = "";
-	var suitFour = "";
+	//purge the variables here
+}
+function messagefadeOut(message,time,type){
+	$(".message").removeClass("message-alert").removeClass("message-success");
+	if(time === undefined){ time = 2000;}
+	if(time != undefined){$(".message").addClass("message-"+type);}
+	$(".message").html("").fadeIn();
+	$(".message").html(message).fadeOut(time);
 }
 function createDeck(){
 	var myDeck = new Array();//create the deck
 	var myObDeck = new Array();
 	for(i=0;i<suits.length;i++){
 		for(j=0;j<startDeck.length;j++){
-			myObDeck.push({"value":startDeck[j],"suit":suits[i]}); //store values as objects
+			myObDeck.push({"value":startDeck[j],"suit":suits[i],"placeholder":myObDeck.length}); 
+			//store values as objects
 		}
 	}
 	var m = myObDeck.length, t, i;//now shuffle it using the Fisher-Yates
@@ -57,10 +64,12 @@ function createDeck(){
   	startCard = myObDeck[0];//start building pick out first card
   	line0.push(startCard);
   	suitOne = startCard.suit;
-  	console.log(suitOne);
+  	//console.log(suitOne);
   	
   	removeByIndex(myObDeck,0);//now remove the first card from the array
-  	console.log("startingCard: "+startCard);
+  	//DISPLAY FIRST CARD IN THE LINE UP
+  	$(".suit1 span").html("<div class='"+startCard.suit+"'>"+startCard.value+"</div>");
+  	//console.log("startingCard: "+startCard);
   	var gutterCount = 0;
   	for(x=0;x<4;x++){//then build the columns on the right creating 4 groups of 4 cards
 	  	window['gutter'+x] = [];
@@ -69,16 +78,30 @@ function createDeck(){
 			window['gutter'+x].push(myObDeck[gutterCount]);
 			removeByIndex(myObDeck,gutterCount);
 		}
-		console.log(window['gutter'+x]);
+		//console.log(window['gutter'+x]);
 	}
 	
-  	//console.log("object:" + myObDeck[0].value + " : " + myObDeck[0].suit);
+  
 	//console.log(myObDeck.length);
-	console.log(myObDeck.length);
-	console.log("dealing hand: "+ myObDeck);
+	//console.log("dealing hand: "+ myObDeck);
 	//set up side decks
   	//compile lower dealing deck
-  	return myObDeck;
+  	//return myObDeck;
+  	displayBottomDeck();
+}
+function displayBottomDeck(){
+	for(z=0;z<4;z++){
+		var inserthtml = "";
+		var inactive = "";
+		//console.log(window['gutter'+z].length);
+		for(a=0;a<window['gutter'+z].length;a++){
+			//console.log(window['gutter'+z][1].suit);
+			if(a <= (window['gutter'+z].length)-2){inactive = "inactive";}else{inactive = "";}
+			inserthtml += "<div class='card "+window['gutter'+a][z].suit+" "+inactive+"' id="+window['gutter'+z][a].value + ":" + window['gutter'+a][z].suit+">"+window['gutter'+z][a].value+"</div>";
+		}
+		$(".sidegroup" + (z+1)).html(inserthtml);
+	}
+	
 }
 function dealCards(array){
 	//grab the start card and identify the suit
@@ -88,7 +111,28 @@ function dealCards(array){
 	//removeByIndex(array,0);
 	//then put the remaining into a hand at the bottom and show the first three cards
 }
-
+function checkCanPlace(value){
+	var cardArr = [];
+	cardArr = value.split(":");
+	//lets compare it to the first deck.
+	console.log("suitOne: " + suitOne + "-" + cardArr[1]); 
+	if(cardArr[1] === suitOne){//if it matches the card in the first row move it up there.
+		alert("match");
+		//line0.push[];
+	}else if(cardArr[0] === startCard.value){
+		alert("start new row");
+		//what line do we start
+	}else if(cardArr[0] === "bleh"){//check to see if it can go in line 2
+		
+	}else if(cardArr[0] === "bleh"){//check to see if it can go in line 3
+		
+	}else if(cardArr[0] === "bleh"){//check to see if it can go in line 4
+		
+	}
+	var cardvalue = cardArr[0];
+	var cardsuit = cardArr[1];
+	console.log(cardvalue + ":" + cardsuit);
+}
 
 
 
@@ -105,16 +149,35 @@ function dealCards(array){
 /*SAVE GAME*/
 //save the current status of the game - this should automattically happen if the game goes into background mode.
 /*BUTTON CODE*/
+
+////var result_obj = objectFindByKey(array, 'id', '45');
+
+$('body').on('click', 'div.card', function() {
+	if(!$(this).hasClass("inactive")){ //temp solution to detect if card can be clicked
+    	var cardvalue = $(this).attr("id") + $(this).attr("class");
+    	checkCanPlace($(this).attr("id"),$(this).attr("class"));
+		messagefadeOut(cardvalue,1000);
+	}else{
+		messagefadeOut("Sorry, that card is locked. Please try a different one.",3000,"alert");
+	}
+});
 $(".deal").click(function(){
 	shuffledDeck = createDeck();
 	dealCards(shuffledDeck);
+	/*Interface changes*/
 	$(".deal").addClass('ui-state-disabled');
 	$(".reset").removeClass('ui-state-disabled');
-	//console.log("n:"+ shuffledDeck);
-	//disable the deal button
 });
 $(".reset").click(function(){
 	resetVariables();
+	console.log("Reset");
+	/*Interface changes*/
 	$(".deal").removeClass('ui-state-disabled');
 	$(".reset").addClass('ui-state-disabled');
+});
+$(".save").click(function(){
+	console.log("Save");
+});
+$(".load").click(function(){
+	console.log("Load");
 });
