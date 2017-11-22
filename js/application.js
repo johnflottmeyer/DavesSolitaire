9de4d97659ac7e70e8668d08c3f8c648e88b5cf4
john <john@voilamediagroup.com>
2017-11-22 08:32:18 -0600

    adding in full ads info

--- js/application.js
+++ js/application.js
@@ -17,23 +17,92 @@ Check to see if the start card exists in the bottom decks and if it does make su
 
 ==============================================
 *********************************************/
+ var isAppForeground = true;
+    
+    function initAds() {
+      if (admob) {
+        var adPublisherIds = {
+          ios : {
+            banner : "ca-app-pub-8087701798858995/6405768747",
+            interstitial : "ca-app-pub-XXXXXXXXXXXXXXXX/IIIIIIIIII"
+          },
+          android : {
+            banner : "ca-app-pub-XXXXXXXXXXXXXXXX/BBBBBBBBBB",
+            interstitial : "ca-app-pub-XXXXXXXXXXXXXXXX/IIIIIIIIII"
+          }
+        };
+    	  
+        var admobid = (/(android)/i.test(navigator.userAgent)) ? adPublisherIds.android : adPublisherIds.ios;
+            
+        admob.setOptions({
+          publisherId:      admobid.banner,
+          interstitialAdId: admobid.interstitial,
+          tappxIdiOS:       "/XXXXXXXXX/Pub-XXXX-iOS-IIII",
+          tappxIdAndroid:   "/XXXXXXXXX/Pub-XXXX-Android-AAAA",
+          tappxShare:       0.5,
+          
+        });
+ 
+        registerAdEvents();
+        
+      } else {
+        alert('AdMobAds plugin not ready');
+      }
+    }
+    
+    function onAdLoaded(e) {
+      if (isAppForeground) {
+        if (e.adType === admob.AD_TYPE.INTERSTITIAL) {
+          console.log("An interstitial has been loaded and autoshown. If you want to load the interstitial first and show it later, set 'autoShowInterstitial: false' in admob.setOptions() and call 'admob.showInterstitialAd();' here");
+        } else if (e.adType === admob.AD_TYPE_BANNER) {
+          console.log("New banner received");
+        }
+      }
+    }
+    
+    function onPause() {
+      if (isAppForeground) {
+        admob.destroyBannerView();
+        isAppForeground = false;
+      }
+    }
+    
+    function onResume() {
+      if (!isAppForeground) {
+        setTimeout(admob.createBannerView, 1);
+        setTimeout(admob.requestInterstitialAd, 1);
+        isAppForeground = true;
+      }
+    }
+    
+    // optional, in case respond to events 
+    function registerAdEvents() {
+      document.addEventListener(admob.events.onAdLoaded, onAdLoaded);
+      document.addEventListener(admob.events.onAdFailedToLoad, function (e) {});
+      document.addEventListener(admob.events.onAdOpened, function (e) {});
+      document.addEventListener(admob.events.onAdClosed, function (e) {});
+      document.addEventListener(admob.events.onAdLeftApplication, function (e) {});
+      document.addEventListener(admob.events.onInAppPurchaseRequested, function (e) {});
+      
+      document.addEventListener("pause", onPause, false);
+      document.addEventListener("resume", onResume, false);
+    }
+        
+    function onDeviceReady() {
+      document.removeEventListener('deviceready', onDeviceReady, false);
+      initAds();
+ 
+      // display a banner at startup 
+      admob.createBannerView();
+        
+      // request an interstitial 
+      admob.requestInterstitialAd();
+    }
+    
+    document.addEventListener("deviceready", onDeviceReady, false);
+    
+    
 
-function onDeviceReady() {
-    document.removeEventListener('deviceready', onDeviceReady, false);
-
-    // Set AdMobAds options:
-    admob.setOptions({
-        publisherId:          "ca-app-pub-8087701798858995/6405768747",  // Required pub-2038262597739639
-        interstitialAdId:     "ca-app-pub-XXXXXXXXXXXXXXXX/IIIIIIIIII",  // Optional
-        tappxIdiOS:           "/XXXXXXXXX/Pub-XXXX-iOS-IIII",            // Optional
-        tappxIdAndroid:       "/XXXXXXXXX/Pub-XXXX-Android-AAAA",        // Optional
-        tappxShare:           0.5                                        // Optional
-    });
-    admob.createBannerView();
-     //$(".loadpop").click(); 
-}
-
-document.addEventListener("deviceready", onDeviceReady, false);
 
 $(document).on("pagecreate","#gameboard", function(){ 
     //$("#btnpopup").on("click", function(){
@@ -42,7 +111,7 @@ $(document).on("pagecreate","#gameboard", function(){
         //setTimeout(function(){  $("#p").popup("close"); }, 5000);
     //});
     //$(".loadpop").click(); 
-    
+    $(".loadpop").click(); 
 });
 $(document).on('pageshow', '#gameboard' ,function () {
  //if (getValue() == null) {
