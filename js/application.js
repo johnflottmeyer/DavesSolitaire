c64ed3664184911e64b4522ae2a018bc70d59a34
john <john@voilamediagroup.com>
2018-01-25 12:34:08 -0600

    changing the banner code as a test

--- js/application.js
+++ js/application.js
@@ -45,7 +45,7 @@ function initAds() {
   if (admob) {
     var adPublisherIds = {
       ios : {
-        banner : "ca-app-pub-8087701798858995/6405768747" , //ca-app-pub-8087701798858995~8219034559 //
+        banner : "ca-app-pub-8087701798858995/7121143441" , //ca-app-pub-8087701798858995~8219034559 //
         interstitial : "ca-app-pub-8087701798858995/9554019722"
       },
       android : {
